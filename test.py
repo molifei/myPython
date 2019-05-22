b67f6caaba3bf383ac347fa3f9d98e72ad701c9f
wyk <574579565@qq.com>
2019-05-22 18:09:25 +0800

    init

---
+++ test.py
@@ -0,0 +1,31 @@
+# print("hello world")
+#
+# a = input()
+#
+# if a >= 5:
+#     print("大于")
+# else:
+#     print("小于")
+
+#
+# print(
+#     "i'm david"
+# )
+# print(123)
+#
+# a = True
+# print(a)
+#
+# print(3<2)
+
+# a = 'ABC'
+# b = a
+# a = 'XYZ'
+# print(a,b)
+
+
+# print("n =",123)
+# print(456.789)
+# print("hello,\\'Adam\\'")
+print(r'''hello,\n
+world''')
